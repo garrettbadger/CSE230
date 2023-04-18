bc20502a2b4824bc845b7e310db33fad8a5987af
garrettbadger <garrett.badger.id@gmail.com>
2023-04-18 15:01:21 -0600

    W01 Edit

--- W01/W01/W01.cpp
+++ W01/W01/W01.cpp
@@ -1,14 +1,15 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Garrett Badger
  * 2. Assignment Name:
  *      Lab 01: Hello World
  * 3. Assignment Description:
  *      A simple program to display a message on the screen
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *      The hardest part was getting used to Visual Studio Community and figuring
+ *      out how to get my program to run.
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, and writing code-
+ *      3 hours.
  *****************************************************************/
 
 #include <iostream>
