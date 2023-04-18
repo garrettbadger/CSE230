80b82418971bdba79cc9d4b176d0c6dd5d240d1a
garrettbadger <garrett.badger.id@gmail.com>
2023-04-18 14:59:03 -0600

    W01 edit

--- W01/W01/W01.cpp
+++ W01/W01/W01.cpp
@@ -1,7 +1,20 @@
-
+/*************************************************************
+ * 1. Name:
+ *      -your name-
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      A simple program to display a message on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      -total time in hours: reading the assignment, submitting, and writing code-
+ *****************************************************************/
 
 #include <iostream>
-
+/*
+* This is the Main function which outputs Hello World! to the screen.
+*/
 int main()
 {
     std::cout << "Hello World!\n";
